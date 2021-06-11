7cd58961aa74ff90c281acedd6820968278583d8
dhanvanthsingaravelu <52419369+dhanvanthsingaravelu@users.noreply.github.com>
2021-06-11 19:44:37 +0530

    fin

--- seprojbackend/server.js
+++ seprojbackend/server.js
@@ -28,12 +28,20 @@ const db = knex({
       ssl: true
     }*/
     connection: {
+      host : 'roomoccupancy.postgres.database.azure.com',
+      user : 'dhanvanth@roomoccupancy',
+      password : 'Thudu@123',
+      database : 'roomoccupancy',
+      ssl: true
+    }
+    /*
+    connection: {
       host : '3.108.61.161',
       port: 5432,
       user : 'postgres',
       password : '1077',
       database : 'roomoccupancy',
-    }
+    }*/
   });
 
 
